468aa5f2ff1e5e17f26873bc33b1f8c352e3a7cf
Alvaro Gonzalez <alvarogonzalezsancha@gmail.com>
2016-12-29 10:31:01 +0100

    prueba

---
+++ ProgramacionJEE/src/ejercicios/prueba2.java
@@ -0,0 +1,22 @@
+#HttpServlet()
+     */
+    public prueba2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
